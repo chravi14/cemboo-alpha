ce4c99e731f0e006905699627e9e491a0d8cfbb9
chravi14 <ravindra@haydenfilmsinstitute.org>
2022-01-06 20:18:34 -0600

    resolved conflict

--- src/pages/route-landing/landing-page.tsx
+++ src/pages/route-landing/landing-page.tsx
@@ -1,7 +1,23 @@
+import {
+  CustomHeader,
+  CustomSubHeader,
+  CustomBoldText,
+  CustomLightText,
+} from "../../components/ui/typography";
 import * as Styled from "./landing-page.styled";
 
 export const LandingPage = () => {
   return (
-    <Styled.LandingPageWrapper>It is landing Page</Styled.LandingPageWrapper>
+    <Styled.LandingPageWrapper>
+      <CustomHeader>Haydenfilms 1.0 Screenplay Competition</CustomHeader>
+      <CustomSubHeader> Meet the Judges</CustomSubHeader>
+      <CustomBoldText>Viktoria Decker</CustomBoldText>
+      <CustomLightText size={14}>Senior Vice President,</CustomLightText>
+      <CustomLightText size={16}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam.
+      </CustomLightText>
+    </Styled.LandingPageWrapper>
   );
 };
